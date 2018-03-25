91fb5e6f7139ef9d417c3a9956b0451fd0d1561d
Robin <3327864+rkeet@users.noreply.github.com>
2018-03-25 21:21:23 +0200

    Create Module.php

---
+++ src/Module.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Keet\Form\Examples;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface, AutoloaderProviderInterface
+{
+    /**
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        $config = [];
+
+        foreach (glob(dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . '*.php') as $filename) {
+            $config = array_merge_recursive($config, include $filename);
+        }
+
+        return $config;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAutoloaderConfig(): array
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__,
+                ],
+            ],
+        ];
+    }
+}
