8bc0fed9408530c7a81a4258459c9b8f3d66a432
Robin Keet <robin@keet.me>
2018-03-31 10:17:50 +0200

    Created config for all of the classes, the module and the form, fieldset and input filter usage

--- config/forms.config.php
+++ config/forms.config.php
@@ -2,36 +2,54 @@
 
 namespace Keet\Form\Examples;
 
-use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Keet\Form\Examples\Factory\Fieldset\AddressFieldsetFactory;
+use Keet\Form\Examples\Factory\Fieldset\CityFieldsetFactory;
+use Keet\Form\Examples\Factory\Fieldset\CoordinatesFieldsetFactory;
+use Keet\Form\Examples\Factory\Form\AddressFormFactory;
+use Keet\Form\Examples\Factory\Form\CityFormFactory;
+use Keet\Form\Examples\Factory\Form\CoordinatesFormFactory;
+use Keet\Form\Examples\Factory\InputFilter\Fieldset\AddressFieldsetInputFilterFactory;
+use Keet\Form\Examples\Factory\InputFilter\Fieldset\CityFieldsetInputFilterFactory;
+use Keet\Form\Examples\Factory\InputFilter\Fieldset\CoordinatesFieldsetInputFilterFactory;
+use Keet\Form\Examples\Factory\InputFilter\Form\AddressFormInputFilterFactory;
+use Keet\Form\Examples\Factory\InputFilter\Form\CityFormInputFilterFactory;
+use Keet\Form\Examples\Factory\InputFilter\Form\CoordinatesFormInputFilterFactory;
+use Keet\Form\Examples\Fieldset\AddressFieldset;
+use Keet\Form\Examples\Fieldset\CityFieldset;
+use Keet\Form\Examples\Fieldset\CoordinatesFieldset;
+use Keet\Form\Examples\Form\AddressForm;
+use Keet\Form\Examples\Form\CityForm;
+use Keet\Form\Examples\Form\CoordinatesForm;
+use Keet\Form\Examples\InputFilter\Fieldset\AddressFieldsetInputFilter;
+use Keet\Form\Examples\InputFilter\Fieldset\CityFieldsetInputFilter;
+use Keet\Form\Examples\InputFilter\Fieldset\CoordinatesFieldsetInputFilter;
+use Keet\Form\Examples\InputFilter\Form\AddressFormInputFilter;
+use Keet\Form\Examples\InputFilter\Form\CityFormInputFilter;
+use Keet\Form\Examples\InputFilter\Form\CoordinatesFormInputFilter;
 
 return [
-    'controllers' => [
+    'form_elements' => [
         'factories' => [
-            CitiesController::class => CitiesControllerFactory::class,
+            AddressForm::class => AddressFormFactory::class,
+            AddressFieldset::class => AddressFieldsetFactory::class,
+
+            CityForm::class => CityFormFactory::class,
+            CityFieldset::class => CityFieldsetFactory::class,
+
+            CoordinatesForm::class => CoordinatesFormFactory::class,
+            CoordinatesFieldset::class => CoordinatesFieldsetFactory::class,
         ],
     ],
-    'doctrine' => [
-        'driver' => [
-            __NAMESPACE__ . '_driver' => [
-                'class' => AnnotationDriver::class,
-                'cache' => 'array',
-                'paths' => [
-                    __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Entity',
-                ]
-            ],
-            'orm_default' => [
-                'drivers' => [
-                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
-                ],
-            ],
-        ],
-    ],
-    'view_manager' => [
-        'template_map' => [
-            'blog/partials/blog/blogForm' => __DIR__ . '/../view/blog/partials/blog-form.phtml',
-        ],
-        'template_path_stack' => [
-            __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'view',
+    'input_filters' => [
+        'factories' => [
+            AddressFormInputFilter::class => AddressFormInputFilterFactory::class,
+            AddressFieldsetInputFilter::class => AddressFieldsetInputFilterFactory::class,
+
+            CityFormInputFilter::class => CityFormInputFilterFactory::class,
+            CityFieldsetInputFilter::class => CityFieldsetInputFilterFactory::class,
+
+            CoordinatesFormInputFilter::class => CoordinatesFormInputFilterFactory::class,
+            CoordinatesFieldsetInputFilter::class => CoordinatesFieldsetInputFilterFactory::class,
         ],
     ],
 ];
--- config/module.config.php
+++ config/module.config.php
@@ -2,4 +2,47 @@
 
 namespace Keet\Form\Examples;
 
-return [];
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Keet\Form\Examples\Controller\AddressController;
+use Keet\Form\Examples\Controller\CityController;
+use Keet\Form\Examples\Controller\CoordinatesController;
+use Keet\Form\Examples\Factory\Controller\AddressControllerFactory;
+use Keet\Form\Examples\Factory\Controller\CityControllerFactory;
+use Keet\Form\Examples\Factory\Controller\CoordinatesControllerFactory;
+
+return [
+    'controllers' => [
+        'factories' => [
+            AddressController::class => AddressControllerFactory::class,
+            CityController::class => CityControllerFactory::class,
+            CoordinatesController::class => CoordinatesControllerFactory::class,
+        ],
+    ],
+    'doctrine' => [
+        'driver' => [
+            __NAMESPACE__ . '_driver' => [
+                'class' => AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [
+                    __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Entity',
+                ]
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                ],
+            ],
+        ],
+    ],
+    'view_manager' => [
+        'controller_map' => [
+            'Keet\Form\Examples' => 'keet',
+        ],
+        'template_map' => [
+            'keet/partials/coordinates/coordinatesForm' => __DIR__ . '/../view/partials/coordinates/coordinates-form.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . DIRECTORY_SEPARATOR .'..' . DIRECTORY_SEPARATOR . 'view',
+        ],
+    ],
+];
